25153d3b39c7b40e3b1c3c7e9944f2281300fe26
Iain Woodburn <enjkw13@gmail.com>
2016-07-22 08:49:32 -0400

    Update Penny.java
    Added sanitization to the player string

--- Penny.java
+++ Penny.java
@@ -1,24 +1,54 @@
 import java.util.Scanner;
 
 public class Penny{
+  
   public static void main(String[] args){
     Scanner scan = new Scanner();
     
     //Placeholder for playerSeq, makes sure that the length is 3
     String playerSeq = "XXX";
+    boolean sanitized = false;
     
-    while(playerSeq.length() == 3){ 
+    //Continues until the player enters a string of length 3
+    //and all of the characters are of type 'T' or 'H'
+    while(playerSeq.length() == 3 && !sanitized){ 
       //Gets the sequence of tosses from the player
       System.out.println("Enter a sequence of three flips (H-Heads, T-Tails)");
       String playerSeq = scan.nextLine();
+      
+      //Converts to uppercase, everything should be uppercase past this point
+      playerSeq = playerSeq.toUppercase();
+      
+      char playerFirst = playerSeq.charAt(0);
+      char playerSecond = playerSeq.charAt(1);
+      char playerThird = playerSeq.charAt(2);
+      
+      //Checks first character, to see if it is at least a 'T' or 'H'
+      if(playerFirst != 'T' && playerFirst != 'H'){
+        sanitized = false;
+        continue;
+      }else{
+        sanitized = true;
+      }
+      
+      //Checks second character, just like the first
+      if(playerSecond != 'T' && playerSecond != 'H'){
+        sanitized = false;
+        continue;
+      }
+      
+      //Checks third character, just like the first
+      if(playerThird != 'T' && playerThird != 'H'){
+        sanitized = false;
+        continue;
+      }
+      
+    }//end while
     
-    }
+  }//end main
+  
+  public static String calculateBetterSeq(String seq){
     
-    
-    char playerFirst = playerSeq.charAt(0);
-    char playerSecond = playerSeq.charAt(1);
-    char playerThird = playerSeq.charAt(2);
-    
-    
-  }
-}
+  }//end calculateBetterSeq
+  
+}//end class
