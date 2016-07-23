4b2e807543cb40cac0cb26eef85aa9d9beb61caa
Iain Woodburn <enjkw13@gmail.com>
2016-07-23 10:46:29 -0500

    Update Penny.java
    Added methods: 
      -calculateBetterSeq: calculates a better sequence of flips
      -generateRandomFlip: randomly generates heads or tails, for actually playing the game
    Started game, with scoring and number of wins

--- Penny.java
+++ Penny.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Penny{
   
   public static void main(String[] args){
-    Scanner scan = new Scanner();
+    Scanner scan = new Scanner(System.in);
     
     //Placeholder for playerSeq, makes sure that the length is 3
     String playerSeq = "XXX";
     boolean sanitized = false;
+    int playerScore = 0;
+    int computerScore = 0;
+    int playerWins = 0;
+    int computerWins = 0;
     
     //Continues until the player enters a string of length 3
     //and all of the characters are of type 'T' or 'H'
@@ -45,10 +50,63 @@ public class Penny{
       
     }//end while
     
+    //Creates a computer sequence that beats the player sequence
+    String computerSeq = calculateBetterSeq(playerFirst, playerSecond, playerThird);
+    boolean done = false;
+    String randomSeq = "";
+
+    while(!done){
+
+      randomSeq += generateRandomFlip();
+
+      if(queue.size() == 3){
+        //Shortens the string to 3 characters, removing the first
+        //and keeping the last one added
+        randomSeq = randomSeq.substring(1 , randomSeq.length());
+
+        //Checks if a player has won
+        if(randomSeq.equals(playerSeq)){
+          playerScore++;
+        }else if(randomSeq.equals(computerSeq)){
+          computerScore++;
+        }
+
+      }
+
+      //Adds a win to either the player or computer 
+      if(playerScore == 3){
+          playerWins++;
+      }else if(computerScore == 3){
+          computerWins++;
+      }
+    
   }//end main
   
-  public static String calculateBetterSeq(String seq){
-    
+  public static String calculateBetterSeq(char first, char second, char third){
+
+    if(second == 'T'){
+      second = 'H';
+    }else{
+      second = 'T';
+    }
+    //Takes the second character, flips it, and puts it on the front
+    String result = second + first + third;
+    return result;
   }//end calculateBetterSeq
+
+  public static char generateRandomFlip(){
+    Random rand = new Random();
+
+    //Random number from 0 to 1, which will be converted into a char
+    int numFlip = rand.nextInt(1);
+
+    //If num is 0, H(Heads) is returned, if 1, then T(Tails) is returned
+    if(numFlip == 0){
+      return 'H';
+    }else{
+      return 'T';
+    }
+
+  }//end generateRandomFlip
   
 }//end class
